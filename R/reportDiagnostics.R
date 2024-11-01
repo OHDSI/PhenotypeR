6a8521e03c270dffba456439756aa74ac1a22e20
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-11-01 21:01:17 +0000

    all deps from cran

--- R/reportDiagnostics.R
+++ R/reportDiagnostics.R
@@ -1,30 +1,24 @@
-#' Create a report summarising your phenotyping results
-#'
-#' @inheritParams resultDoc
-#' @inheritParams directoryDoc
-#'
-#' @return A report
-#' @noRd
-reportDiagnostics <- function(result,
-                              directory = here::here()){
 
-    input <- system.file("rmd", "phenotype_report.Rmd",
-                         package = "PhenotypeR")
-
-    cohortNames <- result |>
-      visOmopResults::addSettings() |>
-      omopgenerics::filter(.data$result_type == "summarised_characteristics") |>
-      dplyr::filter(.data$group_level != "overall") |>
-      dplyr::select("group_level") |>
-      dplyr::distinct() |>
-      dplyr::pull()
-    cohortNames <- paste0(cohortNames, collapse = "; ")
-    workingTitle <- paste('PhenotypeR results for cohort', cohortNames)
-
-    rmarkdown::render(input = input,
-                      params = list(title = workingTitle,
-                                    result = result),
-                      output_file = "report.html",
-                      output_dir = directory,
-                      clean = TRUE)
-}
+# reportDiagnostics <- function(result,
+#                               directory = here::here()){
+#
+#     input <- system.file("rmd", "phenotype_report.Rmd",
+#                          package = "PhenotypeR")
+#
+#     cohortNames <- result |>
+#       visOmopResults::addSettings() |>
+#       omopgenerics::filter(.data$result_type == "summarised_characteristics") |>
+#       dplyr::filter(.data$group_level != "overall") |>
+#       dplyr::select("group_level") |>
+#       dplyr::distinct() |>
+#       dplyr::pull()
+#     cohortNames <- paste0(cohortNames, collapse = "; ")
+#     workingTitle <- paste('PhenotypeR results for cohort', cohortNames)
+#
+#     rmarkdown::render(input = input,
+#                       params = list(title = workingTitle,
+#                                     result = result),
+#                       output_file = "report.html",
+#                       output_dir = directory,
+#                       clean = TRUE)
+# }
