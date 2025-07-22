670a8178b6de883d6ea2b5fd403acc855c5fd927
catalamarti <marticatalasabate@gmail.com>
2025-07-22 22:37:55 +0100

    clean

--- extras/docShiny.R
+++ extras/docShiny.R
@@ -1,12 +1,3 @@
 
-rsconnect::setAccountInfo(
-  name = "dpa-pde-oxford",
-  token = Sys.getenv("SHINYAPPS_TOKEN"),
-  secret = Sys.getenv("SHINYAPPS_SECRET")
-)
-rsconnect::deployApp(
-  appDir = file.path(getwd(), "PhenotypeRShiny"),
-  appName = "PhenotypeRShiny",
-  forceUpdate = TRUE,
-  logLevel = "verbose"
-)
+appName <- "PhenotypeRShiny"
+source(here::here("extras", "deployShiny.R"))
--- extras/testShiny.R
+++ extras/testShiny.R
@@ -1,12 +1,3 @@
 
-rsconnect::setAccountInfo(
-  name = "dpa-pde-oxford",
-  token = Sys.getenv("SHINYAPPS_TOKEN"),
-  secret = Sys.getenv("SHINYAPPS_SECRET")
-)
-rsconnect::deployApp(
-  appDir = file.path(getwd(), "PhenotypeRShiny"),
-  appName = "PhenotypeRShinyTest",
-  forceUpdate = TRUE,
-  logLevel = "verbose"
-)
+appName <- "PhenotypeRShinyTest"
+source(here::here("extras", "deployShiny.R"))
