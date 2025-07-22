1030086f49a4764547a8c1bb8313624c7e442b31
catalamarti <marticatalasabate@gmail.com>
2025-07-22 16:21:58 +0100

    shiny

--- extras/testShiny.R
+++ extras/testShiny.R
@@ -1,2 +1,12 @@
-appName <- "PhenotypeRShinyTest"
-source(here::here("extras", "deployShiny.R"))
+
+rsconnect::setAccountInfo(
+  name = "dpa-pde-oxford",
+  token = Sys.getenv("SHINYAPPS_TOKEN"),
+  secret = Sys.getenv("SHINYAPPS_SECRET")
+)
+rsconnect::deployApp(
+  appDir = file.path(getwd(), "PhenotypeRShiny"),
+  appName = "PhenotypeRShinyTest",
+  forceUpdate = TRUE,
+  logLevel = "verbose"
+)
