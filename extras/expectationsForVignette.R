5879f3c4da14426167eeae4b4187b3be1fb0d500
Ed Burn <9583964+edward-burn@users.noreply.github.com>
2025-07-29 16:40:11 +0100

    ellmer 0.3
    closes #425

--- extras/expectationsForVignette.R
+++ extras/expectationsForVignette.R
@@ -1,4 +1,4 @@
-chat <- ellmer::chat_google_gemini()
+chat <- ellmer::chat("google_gemini")
 llm_expectation <- chat$chat(
   ellmer::interpolate("What are the typical characteristics we can expect to see in our real-world data for a cohort of people with an ankle sprain (average age, proportion male vs female, subsequent medications, etc)? Be brief and provide summar with a few sentences."))
 
@@ -14,3 +14,11 @@ readr::write_csv(x = getCohortExpectations(chat = chat,
                                      "diagnosis_of_prostate_cancer",
                                      "new_user_of_morphine")),
                  file = here::here("vignettes", "vignette_phenotype_expectations", "expectations_2.csv"))
+
+chat <- ellmer::chat("mistral")
+readr::write_csv(x = getCohortExpectations(chat = chat,
+                                           phenotypes = c("diagnosis_of_ankle_sprain",
+                                                          "diagnosis_of_prostate_cancer",
+                                                          "new_user_of_morphine")),
+                 file = here::here("vignettes", "vignette_phenotype_expectations", "expectations_3.csv"))
+
