8752f2fd73e732e94e8b147862c80e1ed99226ca
Ed Burn <9583964+edward-burn@users.noreply.github.com>
2025-11-26 19:31:15 +0000

    tidy obs period table

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -149,7 +149,8 @@ server <- function(input, output, session) {
 
     result <- dataFiltered$summarise_observation_period |>
       dplyr::filter(cdm_name %in% shared_cdm_names())
-
+    attr(result, "settings")  <- attr(result, "settings") |>
+      dplyr::select(!c("diagnostic", "phenotyper_version"))
     validateFilteredResult(result)
 
     return(result)
